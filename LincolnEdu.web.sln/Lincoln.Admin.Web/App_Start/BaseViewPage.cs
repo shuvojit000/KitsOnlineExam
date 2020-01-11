cbf3447792602f9ee85982247887635878cd027d
Shuvojit Halder <shuvojit000@gmail.com>
2020-01-11 13:00:13 +0530

    Add Files

---
+++ LincolnEdu.web.sln/Lincoln.Admin.Web/App_Start/BaseViewPage.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Web.Mvc;
+
+namespace Lincoln.Admin.Web.App_Start
+{
+    public abstract class BaseViewPage : WebViewPage
+    {
+        public virtual new CustomPrincipal User
+        {
+            get { return base.User as CustomPrincipal; }
+        }
+    }
+    public abstract class BaseViewPage<TModel> : WebViewPage<TModel>
+    {
+        public virtual new CustomPrincipal User
+        {
+            get { return base.User as CustomPrincipal; }
+        }
+    }
+}
--- LincolnEdu.web.sln/Lincoln.Admin.Web/App_Start/RouteConfig.cs
+++ LincolnEdu.web.sln/Lincoln.Admin.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Lincoln.Admin.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "LogIn", id = UrlParameter.Optional }
             );
         }
     }
