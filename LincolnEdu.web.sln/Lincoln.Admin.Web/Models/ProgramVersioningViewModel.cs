c5132abba5bcd01cf4d95f4df68215aefc3d4034
shuvojit000 <57279062+shuvojit000@users.noreply.github.com>
2020-01-22 19:27:59 +0530

    Add files via upload

---
+++ LincolnEdu.web.sln/Lincoln.Admin.Web/Models/ProgramVersioningViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lincoln.Admin.Web.Models
+{
+    public class ProgramVersioningViewModel
+    {
+        public int ProgramVersioningID { get; set; }
+        public int DepartmentCode { get; set; }
+        public int ProgramCode { get; set; }
+        public string Version { get; set; }
+        public string PlaceHolder { get; set; }
+        public string Credit { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public string Active { get; set; }
+        public List<SelectListItem> ProgramList { get; set; }
+        public List<SelectListItem> DepartmentList { get; set; }
+    }
+}
---
+++ LincolnEdu.web.sln/Lincoln.Admin.Web/Models/ProgrammeSemesterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lincoln.Admin.Web.Models
+{
+    public class ProgrammeSemesterViewModel
+    {
+        public int? ProgrammeSemesterID { get; set; }
+        public int? AcademicID { get; set; }
+        public int? DepartmentID { get; set; }
+        public int? ProgrammeID { get; set; }
+        public int? CountryID { get; set; }
+        public string AcademicName { get; set; }
+        public string DepartmentName { get; set; }
+        public string ProgrammeName { get; set; }
+        public int? ProgrammeYear { get; set; }
+        public int? ProgrammeSemester { get; set; }
+        public string SemesterType { get; set; }
+        public string Status { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+
+        public List<SelectListItem> DepartmentList { get; set; }
+        public List<SelectListItem> ProgrammeList { get; set; }
+        public List<SelectListItem> CountryList { get; set; }
+        public List<SelectListItem> ProgYearList { get; set; }
+        public List<SelectListItem> ProgSEMList { get; set; }
+        public string Active { get; set; }
+    }
+}
