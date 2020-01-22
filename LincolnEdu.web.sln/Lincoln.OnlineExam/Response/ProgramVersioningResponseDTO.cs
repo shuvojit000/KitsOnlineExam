aa74e6c9c8d59a3dc43d5042fbe481f5d1e27d40
shuvojit000 <57279062+shuvojit000@users.noreply.github.com>
2020-01-22 19:48:24 +0530

    Add files via upload

---
+++ LincolnEdu.web.sln/Lincoln.OnlineExam/Response/ProgramVersioningResponseDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lincoln.OnlineExam.Response
+{
+   public class ProgramVersioningResponseDTO
+    {
+        public int ProgramVersioningID { get; set; }
+        public int DepartmentCode { get; set; }
+        public int ProgramCode { get; set; }
+        public string DepartmentName { get; set; }
+        public string ProgramName { get; set; }
+        public string Version { get; set; }
+        public string PlaceHolder { get; set; }
+        public string Credit { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public string Active { get; set; }
+        public string Status { get; set; }
+    }
+}
