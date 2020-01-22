bec19e7ba1b203867e06dedabc11e33f9651ab56
shuvojit000 <57279062+shuvojit000@users.noreply.github.com>
2020-01-22 19:26:16 +0530

    Add files via upload

---
+++ LincolnEdu.web.sln/Lincoln.OnlineExam/Request/ProgramVersioningRequestDTO.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lincoln.OnlineExam.Request
+{
+
+
+    public class ProgramVersioningRequestDTO
+    {
+        public int ProgramVersioningID { get; set; }
+        public int DepartmentCode { get; set; }
+        public int ProgramCode { get; set; }
+        public string DepartmentName { get; set; }
+        public string ProgramName { get; set; }
+        public string Version { get; set; }
+        public string PlaceHolder { get; set; }
+        public string Credit { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public string Active { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ LincolnEdu.web.sln/Lincoln.OnlineExam/Request/ProgrammeSemesterRequestDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lincoln.OnlineExam.Request
+{
+    public class ProgrammeSemesterRequestDTO
+    {
+        public int? ProgrammeSemesterID { get; set; }
+        public int? DepartmentID { get; set; }
+        public int? ProgrammeID { get; set; }
+        public int? CountryID { get; set; }
+        public int? ProgrammeYear { get; set; }
+        public int? ProgrammeSemester { get; set; }
+        public string SemesterType { get; set; }
+        public string Active { get; set; }
+        public int? CreatedBy { get; set; }
+        public int Status { get; set; }
+
+    }
+}
