29054c5f6cee7d48fe9797786e6e111d7abb5411
shuvojit000 <57279062+shuvojit000@users.noreply.github.com>
2020-01-27 18:56:23 +0530

    Add files via upload

--- LincolnEdu.web.sln/Lincoln.OnlineExam/Request/ExaminationSectionRequestDTO.cs
+++ LincolnEdu.web.sln/Lincoln.OnlineExam/Request/ExaminationSectionRequestDTO.cs
@@ -8,23 +8,26 @@ namespace Lincoln.OnlineExam.Request
 {
     public class ExaminationSectionRequestDTO
     {
-        public int ExaminationSectionID { get; set; }
-        public int ProgramCode { get; set; }
+        public int? ExaminationSectionID { get; set; }
+        public int? ProgramCode { get; set; }
         public string ProgramName { get; set; }
-        public int FacultyCode { get; set; }
+        public int? FacultyCode { get; set; }
         public string FacultyName { get; set; }
-        public int SyllabusVersionCode { get; set; }
+        public int? SyllabusVersionCode { get; set; }
         public string SyllabusVersionName { get; set; }
-        public int CountryCode { get; set; }
+        public int? CountryCode { get; set; }
         public string CountryName { get; set; }
-        public int AcademicYearCode { get; set; }
+        public int? AcademicYearCode { get; set; }
         public string YearName { get; set; }
-        public int SemisterCode { get; set; }
+        public int? SemisterCode { get; set; }
         public string SemisterName { get; set; }
-        public int CourseCode { get; set; }
+        public int? SectionID { get; set; }
+        public string SectionName { get; set; }
+        public string CourseCode { get; set; }
+        public int? CourseID { get; set; }
         public string CourseName { get; set; }
         public string Active { get; set; }
-        public int CreatedBy { get; set; }
+        public int? CreatedBy { get; set; }
         public string Status { get; set; }
         public DateTime? CreatedOn { get; set; }
         public int? ModifiedBy { get; set; }
