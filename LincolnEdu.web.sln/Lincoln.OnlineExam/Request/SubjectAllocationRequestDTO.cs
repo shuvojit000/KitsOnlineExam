1ea61fb1331d63cfb282b4a469b4db2f837fb0b0
shuvojit000 <57279062+shuvojit000@users.noreply.github.com>
2020-02-14 15:06:21 +0530

    Add files via upload

--- LincolnEdu.web.sln/Lincoln.OnlineExam/Request/SubjectAllocationRequestDTO.cs
+++ LincolnEdu.web.sln/Lincoln.OnlineExam/Request/SubjectAllocationRequestDTO.cs
@@ -3,43 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Lincoln.OnlineExam.Request
 {
     public class SubjectAllocationRequestDTO
     {
-        public int SubjectAllocationID { get; set; }
-        public int ProgrammeID { get; set; }
-       // public string ProgramName { get; set; }
-        public int EmployeeID { get; set; }
-       // public string FacultyName { get; set; }
-       // public int Version { get; set; }
-        public string Version { get; set; }
-        public int CountryID { get; set; }
-       // public string CountryName { get; set; }
-        public int AcademicID { get; set; }
-       // public string Version { get; set; }
-       // public int SemisterCode { get; set; }
-      //  public string SemisterName { get; set; }
-       // public int CourseCode { get; set; }
-       // public string CourseName { get; set; }
-        public string Active { get; set; }
-        public int CreatedBy { get; set; }
+        public int? SubjectAllocationID { get; set; }
+        public int? ProgrammeID { get; set; }
+        public string ProgramName { get; set; }
+        public int? EmployeeID { get; set; }
+        public string FacultyName { get; set; }
+        public int? Version { get; set; }
+        public string SyllabusVersionName { get; set; }
+        public int? CountryID { get; set; }
+        public string CountryName { get; set; }
+        public int? AcademicID { get; set; }
+        public string YearName { get; set; }
+        public int? CreatedBy { get; set; }
         public string Status { get; set; }
+        public string Active { get; set; }
         public DateTime? CreatedOn { get; set; }
         public int? ModifiedBy { get; set; }
         public DateTime? ModifiedOn { get; set; }
-        public string AllocationDetails { get; set; }
-        public List<SubjectAllocationtDetails> SubAllocationDetails { get; set; }
+        public List<SelectListItem> ProgramList { get; set; }
+        public List<SelectListItem> AcademicYearList { get; set; }
+        public List<SelectListItem> CountryList { get; set; }
+        public List<SelectListItem> SyllabusVersionList { get; set; }
+        public List<SelectListItem> FacultyList { get; set; }
+        public SubjectAllocationList AllocationList { get; set; }
+        public List<SubjectAllocationList> SubAllocationList { get; set; }
+        public string TabAllocationDetails { get; set; }
+        public List<SubjectAllocationDetailsList> SubAllocationDetailsList { get; set; }
     }
-    public class SubjectAllocationtDetails
+
+    public class SubjectAllocationList
     {
-        public int SubjectAllocationDetailsID { get; set; }
         public int SlNo { get; set; }
-        public string AssessmentName { get; set; }
-        public decimal Assessment { get; set; }
-        public string AssessmentType { get; set; }
-        public decimal FullMarks { get; set; }
-        public int OpenClose { get; set; }
+        public int SubjectAllocationID { get; set; }
+        public int? SemisterID { get; set; }
+        public string SemisterName { get; set; }
+        public SubjectAllocationDetailsList AllocationDetails { get; set; }
+        public List<SubjectAllocationDetailsList> SubAllocationDetailsList { get; set; }
+    }
+
+
+    public class SubjectAllocationDetailsList
+    {
+        public int SlNo { get; set; }
+        public string SubjectAllocationDetailsID { get; set; }
+        public int? CourseID { get; set; }
+        public int? ProgrammeSemesterID { get; set; }
+        public string CourseName { get; set; }
+        public int Allocation { get; set; }
     }
 }
